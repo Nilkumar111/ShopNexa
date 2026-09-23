3b63f7b01f6a01f8c721bf22deca04f92ddd35fb
Nilkumar111 <nilesh.raut111@gmail.com>
2026-09-23 15:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -246,23 +246,23 @@ def register():
         if role not in ('customer','supplier'): role='customer'
         approved=1 if role=='customer' else 0
         c=db()
-          try:
-        c.execute(
-            'INSERT INTO users(name,email,password,role,approved) VALUES(%s,%s,%s,%s,%s)',
-            (
-                f['name'].strip(),
-                f['email'].strip().lower(),
-                generate_password_hash(f['password']),
-                role,
-                approved
-            )
+try:
+    c.execute(
+        'INSERT INTO users(name,email,password,role,approved) VALUES(%s,%s,%s,%s,%s)',
+        (
+            f['name'].strip(),
+            f['email'].strip().lower(),
+            generate_password_hash(f['password']),
+            role,
+            approved
         )
+  )
         flash('Supplier account is awaiting admin approval.' if role=='supplier' else 'Account created. Please login.','ok')
         return redirect('/login')
     except psycopg.errors.UniqueViolation:
         flash('Email already registered.','err')
     finally:
-        c.close()
+        c.close()    
     return render_template('register.html')
 
 
